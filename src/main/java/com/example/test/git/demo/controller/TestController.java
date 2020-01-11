e801f022645cb0a4df995099a570a7365e345a26
chenwz <373785730@qq.com>
2020-01-11 20:42:48 +0800

    添加TestController 添加test方法

---
+++ src/main/java/com/example/test/git/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.test.git.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "success";
+    }
+}
